00f43ed1dcfef86ab4b90d48e275006401f37b54
Tshikovha28 <vhenganiem28@gmail.com>
2023-12-15 10:38:33 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,43 +1,25 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * convert_binary_to_uint - Converts a binary number to an unsigned int.
- * @binary_str: Pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: Converted number, 0 if invalid char or if binary_str is NULL.
+ * Return: the converted number
  */
-unsigned int convert_binary_to_uint(const char *binary_str)
+unsigned int binary_to_uint(const char *b)
 {
-	if (binary_str == NULL)
+	int i;
+	unsigned int dec_val = 0;
+
+	if (!b)
 		return (0);
 
-	unsigned int result = (0);
-
-	while (*binary_str != '\0')
+	for (i = 0; b[i]; i++)
 	{
-		if (*binary_str == '0' || *binary_str == '1')
-		{
-			result = result * 2 + (*binary_str - '0');
-		}
-		else
-		{
-			/* Invalid character encountered */
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
-		binary_str++;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
-	return (result);
-}
-
-int main(void)
-{
-	const char *binary_data = "110110";
-	unsigned int converted_value = convert_binary_to_uint(binary_data);
-
-	printf("Binary: %s\n", binary_data);
-	printf("Converted Value: %u\n", converted_value);
-
-	return (0);
+	return (dec_val);
 }
